0824308dc7da45772180be833b18075473d53fa7
JonathanRiche <j.riche64@gmail.com>
2024-10-08 11:10:59 -0400

    fix typo in examples

--- examples/getCampaigns.ts
+++ examples/getCampaigns.ts
@@ -1,7 +1,7 @@
-import { SimplifiClient } from 'simplifi-api-client';
+import { SimplifiClient } from 'simpl.fi-api-client';
 
 const client = new SimplifiClient({
-  apiKey: 'YOUR_API_KEY',
-  apiSecret: 'YOUR_API_SECRET',
+  appApiKey: 'YOUR_API_KEY',
+  userApiKey: 'YOUR_API_SECRET',
   orgId: 'YOUR_ORG_ID',
 });
